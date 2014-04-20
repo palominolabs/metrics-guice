d37e0a9cba5e1551bb63f8e33cae224af880d1f3
Robert Boll <robertcboll@gmail.com>
2014-04-20 00:06:47 -0400

    Adding documentation.

--- src/main/java/com/palominolabs/metrics/guice/BaseInstrumentationModule.java
+++ src/main/java/com/palominolabs/metrics/guice/BaseInstrumentationModule.java
@@ -5,8 +5,12 @@ import com.google.inject.AbstractModule;
 import com.google.inject.matcher.Matchers;
 
 /**
+ * An abstract Guice module which provides a method to instrument methods annotated with the
+ * {@link com.codahale.metrics.annotation.Metered}, {@link com.codahale.metrics.annotation.Timed},
+ * {@link com.codahale.metrics.annotation.Gauge}, and {@link com.codahale.metrics.annotation.ExceptionMetered}
+ * annotations.
  *
- *
+ * @see com.palominolabs.metrics.guice.InstrumentationModule
  */
 public abstract class BaseInstrumentationModule extends AbstractModule {
 
