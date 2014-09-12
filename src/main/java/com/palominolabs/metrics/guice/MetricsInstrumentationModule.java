e2b4b47824f497e92b0ae6747cb9f2a6b0e13a55
clint <clint@magicalspirits.net>
2014-09-12 15:11:55 -0600

    Provide the ability to set the metric registry to child classes of this module

--- src/main/java/com/palominolabs/metrics/guice/MetricsInstrumentationModule.java
+++ src/main/java/com/palominolabs/metrics/guice/MetricsInstrumentationModule.java
@@ -6,12 +6,9 @@ import com.codahale.metrics.annotation.Gauge;
 import com.codahale.metrics.annotation.Metered;
 import com.codahale.metrics.annotation.Timed;
 import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import com.google.inject.matcher.Matcher;
 import com.google.inject.matcher.Matchers;
-import com.google.inject.name.Named;
 
 /**
  * A Guice module which instruments methods annotated with the {@link Metered}, {@link Timed}, {@link Gauge}, and {@link
@@ -28,15 +25,25 @@ import com.google.inject.name.Named;
 public class MetricsInstrumentationModule extends AbstractModule 
 {
     public static final String NAMED_REGISTRY = "MetricsInstrumentationModule.MetricRegistry";
-    protected final MetricRegistry metricRegistry;
+    protected MetricRegistry metricRegistry;
     private final Matcher<? super TypeLiteral<?>> matcher;
 
     /**
      * @param metricRegistry The registry to use when creating meters, etc. for annotated methods.
      */
     public MetricsInstrumentationModule(MetricRegistry metricRegistry) {
-        this.metricRegistry = metricRegistry;
+        this();
+        setRegistry(metricRegistry);
+    }
+    
+    protected MetricsInstrumentationModule()
+    {
         this.matcher = Matchers.any();
+    }
+    
+    protected void setRegistry(MetricRegistry metricRegistry)
+    {
+        this.metricRegistry = metricRegistry;
     }
 
     /**
@@ -54,13 +61,5 @@ public class MetricsInstrumentationModule extends AbstractModule
         bindListener(matcher, new TimedListener(metricRegistry));
         bindListener(matcher, new GaugeListener(metricRegistry));
         bindListener(matcher, new ExceptionMeteredListener(metricRegistry));
-    }
-    
-    @Provides
-    @Named(NAMED_REGISTRY)
-    @Singleton
-    public MetricRegistry providesRegistry()
-    {
-        return this.metricRegistry;
     }
 }
